63576ab134e3d6a332ded14b561e8274fc878241
adelakim <diddnjs02@likelion.org>
2020-12-17 00:33:04 +0900

    4.10 컴포넌트 만들어보기 챌린지

--- React/habit-tracker/src/components/habit.jsx
+++ React/habit-tracker/src/components/habit.jsx
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Habit extends Component {
+  // prop을 보여주기만 하는 컴포넌트이기 때문에 state 필요 ㄴㄴ
+  /*
     state = {
         count: 0,
     }
 
-    handleIncrement = () => {
+    handleIncrement = (count) => {
         // state object 안에 있는 count 증가한 뒤 state update
         // this.state.count++; 이렇게 하면 리액트는 업데이트 됐는지 아닌지 모름 
         // 그래서 state를 업데이트 할때는 반드시 setState를 호출해주어야 함
@@ -16,23 +18,37 @@ class Habit extends Component {
         const count = this.state.count - 1
         this.setState({count: count < 0 ? 0 : count})
     }
-    render() {
-        return (
-            <li className="habit">
-                <span className="habit-name">Reading</span>
-                <span className="habit-count">{this.state.count}</span>
-                <button className="habit-btn habit-increase" onClick={this.handleIncrement}>
-                    <i className="fas fa-plus-square"></i>
-                </button>
-                <button className="habit-btn habit-decrease" onClick={this.handleDecrement}>
-                    <i className="fas fa-minus-square"></i>
-                </button>
-                <button className="habit-btn habit-delete">
-                    <i class="fas fa-trash"></i>
-                </button>
-            </li>
-        )
-    }
+    */
+  handleDecrement = () => {
+    this.props.onDecrement(this.props.habit);
+  };
+
+  handleIncrement = () => {
+    this.props.onIncrement(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
+  render() {
+    // console.log(this.props.habit)
+    const { name, count } = this.props.habit;
+    return (
+      <li className="habit">
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button className="habit-btn habit-increase" onClick={this.handleIncrement}>
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button className="habit-btn habit-decrease" onClick={this.handleDecrement}>
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button className="habit-btn habit-delete" onClick={this.handleDelete}>
+          <i className="fas fa-trash"></i>
+        </button>
+      </li>
+    );
+  }
 }
 
 export default Habit;
